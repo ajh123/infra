4bceb8c5271bdca8ea00962eb8cefc83f7be2468
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2026-01-11 11:54:40 +0000

    feat(traefik): create entrypoint script for Traefik configuration

---
+++ traefik/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+exec traefik \
+  --api.insecure=false \
+  --api.dashboard=true \
+  --providers.docker=true \
+  --providers.docker.exposedbydefault=false \
+  --providers.docker.network=proxy \
+  --entryPoints.web.address=:80 \
+  --entryPoints.websecure.address=:443 \
+  --entryPoints.websecure.http.tls=true \
+  --entryPoints.web.http.redirections.entryPoint.to=websecure \
+  --entryPoints.web.http.redirections.entryPoint.scheme=https \
+  --certificatesresolvers.cf.acme.email="$CLOUDFLARE_EMAIL" \
+  --certificatesresolvers.cf.acme.storage=/letsencrypt/acme.json \
+  --certificatesresolvers.cf.acme.dnschallenge=true \
+  --certificatesresolvers.cf.acme.dnschallenge.provider=cloudflare
